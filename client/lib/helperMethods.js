5ab9eb2500914a070648ffa318149cde745c4f82
Necromos <masternecromos@gmail.com>
2014-03-22 09:37:28 +0100

    init commit

---
+++ client/lib/helperMethods.js
@@ -0,0 +1,8 @@
+Handlebars.registerHelper("setTitle", function(title) {
+	if(typeof title === "string") {
+      document.title = title;  
+    }
+    else {
+      document.title = "Overlay"; 
+    }
+});
